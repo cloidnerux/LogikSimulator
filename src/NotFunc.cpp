162053ee5866f569e139ff856f1af69b29ac7cc8
cloidnerux <cloidnerux@gmail.com>
2014-10-30 14:32:19 +0100

    Added NOT, new example
    Added NOTFunc.
    Implemented a new demo circuit with init phase in main loop and a bitter
    bigger network

---
+++ src/NotFunc.cpp
@@ -0,0 +1,54 @@
+#include "NotFunc.h"
+
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+NOTFunc::NOTFunc(shared_ptr<Node> a, shared_ptr<Node> out, int delay)
+{
+	callCount = 0;
+	inputs.push_back(a);
+	a->assignedFunctions.push_back(this);
+
+	outputs.push_back(make_shared<outputBuffer<bool>>(new outputBuffer<bool>(out.get(), new RingBuffer<bool>(delay))));
+}
+
+NOTFunc::~NOTFunc()
+{
+	inputs.clear();
+	outputs.clear();
+}
+
+void NOTFunc::Update(int cycleTime)
+{
+	bool state = false;
+	if(_cycleTime < cycleTime)
+	{
+		callCount = 0;
+		_cycleTime = cycleTime;
+	}
+	callCount++;
+	callCount %= inputs.size();
+
+	if(inputs.size() < 1)
+		throw "To few inputs for OR operation, minimum is 1";
+	if(outputs.size() != 1)
+		throw "More or less than 1 output for NOT operation!";
+
+	state = !inputs[0].get()->state;
+	outputs[0]->buffer->push_back(state);
+	outputs[0]->output->state = outputs[0]->buffer->GetFirst();
+}
+
+char * NOTFunc::GetType()
+{
+	return "NOT";
+}
+
+char * NOTFunc::GetState()
+{
+	char * state = new char[100];
+	sprintf_s(state, 100, "a = %d, q = %d", inputs[0].get()->state, inputs[1].get()->state, outputs[0].get()->output->state);
+	return state;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "XORFunc.h"
 #include "ORFunc.h"
 #include "ANDFunc.h"
+#include "NotFunc.h"
 
 using namespace std;
 
@@ -16,22 +17,72 @@ int main(int argc, char ** argv)
 	vector<shared_ptr<node<bool>>> nodes;
 	vector<shared_ptr<tFunc>> tFuncs;
 
-	int MAX_CYCLES = 10;
+	int INIT_CYCLES = 7;
+	int SIM_CYCLES = 20;
+	int MAX_CYCLES = SIM_CYCLES + INIT_CYCLES;
+
+	//nodes.push_back(make_shared<node<bool>>(new node<bool>(0)));		//ID 0
+	//nodes.push_back(make_shared<node<bool>>(new node<bool>(0)));		//ID 1
+	//nodes.push_back(make_shared<node<bool>>(new node<bool>(0)));		//ID 2
+	//nodes.push_back(make_shared<node<bool>>(new node<bool>(1)));		//ID 3
+	//nodes.push_back(make_shared<node<bool>>(new node<bool>(1)));		//ID 4
+	//nodes.push_back(make_shared<node<bool>>(new node<bool>(2)));		//ID 5
+	//nodes.push_back(make_shared<node<bool>>(new node<bool>(2)));		//ID 6
+	//nodes.push_back(make_shared<node<bool>>(new node<bool>(3)));		//ID 7
+
+	//tFuncs.push_back(make_shared<XORFunc>(new XORFunc(nodes[0], nodes[1], nodes[3], 2)));
+	//tFuncs.push_back(make_shared<XORFunc>(new XORFunc(nodes[2], nodes[3], nodes[5], 2)));
+	//tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[0], nodes[1], nodes[4], 1)));
+	//tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[2], nodes[3], nodes[6], 1)));
+	//tFuncs.push_back(make_shared<ORFunc>(new ORFunc(nodes[6], nodes[4], nodes[7], 1)));
 
 	nodes.push_back(make_shared<node<bool>>(new node<bool>(0)));		//ID 0
 	nodes.push_back(make_shared<node<bool>>(new node<bool>(0)));		//ID 1
-	nodes.push_back(make_shared<node<bool>>(new node<bool>(0)));		//ID 2
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(1)));		//ID 2
 	nodes.push_back(make_shared<node<bool>>(new node<bool>(1)));		//ID 3
-	nodes.push_back(make_shared<node<bool>>(new node<bool>(1)));		//ID 4
-	nodes.push_back(make_shared<node<bool>>(new node<bool>(2)));		//ID 5
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(3)));		//ID 4
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(3)));		//ID 5
 	nodes.push_back(make_shared<node<bool>>(new node<bool>(2)));		//ID 6
-	nodes.push_back(make_shared<node<bool>>(new node<bool>(3)));		//ID 7
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(2)));		//ID 7
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(5)));		//ID 8
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(5)));		//ID 9
 
-	tFuncs.push_back(make_shared<XORFunc>(new XORFunc(nodes[0], nodes[1], nodes[3], 2)));
-	tFuncs.push_back(make_shared<XORFunc>(new XORFunc(nodes[2], nodes[3], nodes[5], 2)));
-	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[0], nodes[1], nodes[4], 1)));
-	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[2], nodes[3], nodes[6], 1)));
-	tFuncs.push_back(make_shared<ORFunc>(new ORFunc(nodes[6], nodes[4], nodes[7], 1)));
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(4)));		//ID 10
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(4)));		//ID 11
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(6)));		//ID 12
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(6)));		//ID 13
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(7)));		//ID 14
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(7)));		//ID 15
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(8)));		//ID 16
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(8)));		//ID 17
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(9)));		//ID 18
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(9)));		//ID 19
+
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(10)));		//ID 20
+	nodes.push_back(make_shared<node<bool>>(new node<bool>(10)));		//ID 21
+
+	tFuncs.push_back(make_shared<ORFunc>(new ORFunc(nodes[3], nodes[4], nodes[6], 1)));
+	tFuncs.push_back(make_shared<ORFunc>(new ORFunc(nodes[2], nodes[5], nodes[7], 1)));
+	tFuncs.push_back(make_shared<ORFunc>(new ORFunc(nodes[15], nodes[19], nodes[20], 1)));
+	tFuncs.push_back(make_shared<ORFunc>(new ORFunc(nodes[18], nodes[14], nodes[21], 1)));
+	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[0], nodes[6], nodes[5], 1)));
+	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[1], nodes[7], nodes[4], 1)));
+	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[5], nodes[8], nodes[11], 1)));
+	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[4], nodes[9], nodes[10], 1)));
+	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[4], nodes[12], nodes[15], 1)));
+	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[5], nodes[13], nodes[14], 1)));
+
+	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[12], nodes[16], nodes[18], 1)));
+	tFuncs.push_back(make_shared<ANDFunc>(new ANDFunc(nodes[13], nodes[17], nodes[19], 1)));
+	tFuncs.push_back(make_shared<NOTFunc>(new NOTFunc(nodes[1], nodes[3], 1)));
+	tFuncs.push_back(make_shared<NOTFunc>(new NOTFunc(nodes[0], nodes[2], 1)));
+	tFuncs.push_back(make_shared<NOTFunc>(new NOTFunc(nodes[10], nodes[8], 1)));
+	tFuncs.push_back(make_shared<NOTFunc>(new NOTFunc(nodes[11], nodes[9], 1)));
+	tFuncs.push_back(make_shared<NOTFunc>(new NOTFunc(nodes[8], nodes[13], 1)));
+	tFuncs.push_back(make_shared<NOTFunc>(new NOTFunc(nodes[9], nodes[12], 1)));
+	tFuncs.push_back(make_shared<NOTFunc>(new NOTFunc(nodes[15], nodes[16], 1)));
+	tFuncs.push_back(make_shared<NOTFunc>(new NOTFunc(nodes[14], nodes[17], 1)));
+
 
 	auto_ptr<bool> data(new bool[nodes.size() * MAX_CYCLES]);		//In this array we store the state of all nodes for each iteration
 
@@ -43,28 +94,30 @@ int main(int argc, char ** argv)
 	{
 		try
 		{
+			if( cycle >= INIT_CYCLES)
+			{
 			/////////////////////////////////////////////////////////////////////
 			//One way of providing inputs
-			cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
-			cout << "cycle time: " << cycle << endl << endl;
-			cout << "Input state for a (0, 1, keep): ";
-			cin >> s;
-			if(s == "1")
-				nodes[0].get()->state = true;
-			else if(s == "0")
+				cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+				cout << "cycle time: " << cycle << endl << endl;
+				cout << "Input state for a (0, 1, keep): ";
+				cin >> s;
+				if(s == "1")
+					nodes[0].get()->state = true;
+				else if(s == "0")
+					nodes[0].get()->state = false;
+				cout << "Input state for b (0, 1, keep): ";
+				cin >> s;
+				if(s == "1")
+					nodes[1].get()->state = true;
+				else if(s == "0")
+					nodes[1].get()->state = false;
+			}
+			else
+			{
 				nodes[0].get()->state = false;
-			cout << "Input state for b (0, 1, keep): ";
-			cin >> s;
-			if(s == "1")
-				nodes[1].get()->state = true;
-			else if(s == "0")
 				nodes[1].get()->state = false;
-			cout << "Input state for c (0, 1, keep): ";
-			cin >> s;
-			if(s == "1")
-				nodes[2].get()->state = true;
-			else if(s == "0")
-				nodes[2].get()->state = false;
+			}
 			/////////////////////////////////////////////////////////////////////
 
 			//main simulation loop, iterate through every node and update everything
@@ -82,7 +135,7 @@ int main(int argc, char ** argv)
 #endif
 			/////////////////////////////////////////////////////////////////////
 			//output the result for this particular case
-			cout << "Result - q = " << nodes[5].get()->state << ", c = " << nodes[7].get()->state << endl;
+			cout << "Result - q1 = " << nodes[20].get()->state << ", q2 = " << nodes[21].get()->state << endl;
 			/////////////////////////////////////////////////////////////////////
 			//save the actuall node states in an array to analyse later
 			for(unsigned int i = 0; i < nodes.size(); i++)
